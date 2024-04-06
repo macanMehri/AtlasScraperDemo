5bb1a92623a478685fd178d0a6a6f84f8fe1bc0e
Macan Mehri <macanmehri@gmail.com>
2024-04-06 15:41:47 +0330

    Added urls in constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Website addresses
+BASE_URL = 'https://www.atlasobscura.com/'
+
+TRIP_URL = BASE_URL + 'adventures/trips?sort=soonest&reg={continent}'
+
+FOOD_URL = BASE_URL + 'cool-places-to-eat?page={page}'
+
+EXPERIENCES_URL = BASE_URL + 'experiences'
+
+COURSES_URL = BASE_URL + 'online-courses'
+
+STORY_URL = BASE_URL + 'articles'
